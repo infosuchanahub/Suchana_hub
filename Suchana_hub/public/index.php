8f62f1d2e1adcfda291cd81a086311d4e9c65c81
infosuchanahub <info.suchanahub@gmail.com>
2026-03-11 13:36:48 +0545

    Create index.php for attendance system

---
+++ Suchana_hub/public/index.php
@@ -0,0 +1,12 @@
+<?php
+// Main entry point of the application
+
+// Include necessary configuration files
+require_once '../config/config.php';
+
+// Start the session
+session_start();
+
+// Application logic goes here...
+
+?>
