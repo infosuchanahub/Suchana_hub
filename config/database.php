9726b6efccc2f3fdea3a44f377ae4c845a1e7676
infosuchanahub <info.suchanahub@gmail.com>
2026-03-11 13:39:33 +0545

    Create config/database.php with database configuration

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'database' => 'your_database_name',
+    'username' => 'your_username',
+    'password' => 'your_password',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => true,
+];
