1e292ea8538da64f9322124b51eea48b5cb05825
infosuchanahub <info.suchanahub@gmail.com>
2026-03-11 13:39:52 +0545

    Add application constants in config/constants.php

---
+++ config/constants.php
@@ -0,0 +1,10 @@
+<?php
+// Application constants
+
+// Current date and time (UTC)
+define('CURRENT_DATE_TIME', '2026-03-11 07:54:13');
+
+// Current user's login
+
+define('CURRENT_USER_LOGIN', 'infosuchanahub');
+?>
